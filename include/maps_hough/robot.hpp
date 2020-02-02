e0f3a5d0a63ee5d38b5a2ba409c518179889cf23
zmk5 <zkakish@gmail.com>
2020-02-01 20:04:44 -0700

    added neighbor locating client service

--- include/maps_hough/robot.hpp
+++ include/maps_hough/robot.hpp
@@ -28,35 +28,75 @@
 #include "maps_hough/hough.hpp"
 #include "maps_hough/io.hpp"
 #include "maps_hough/manipulatemap.hpp"
+#include "levy_walk_msgs/srv/neighbors.hpp"
+
+
+struct Bounds
+{
+  /*
+   * A structure to store the bounds of the quantities
+   */
+  double max;
+  double min;
+};
+
+
+// A structure to hold range sensor data
+struct LaserSensor
+{
+  /*
+   * A structure to store various entities of a laser range scanner
+   */
+  std::vector<double> bearings;  // various bearing of the laser scanner
+  double range_noise_const;      // variance of the range sensor along the radial direction
+  Bounds range;                  // the max and min range of the sensor
+  Bounds angle;                  // the max and min angle of the sensor
+  std::vector<double> ranges;    // the range along each bearing
+
+};
 
 
 class Robot : public rclcpp::Node
 {
 private:
+  // Robot Attributes
   uint id_;
   std::string name_;
   double sensing_radius_;
-  long int avoid_count, rand_count;
-  sensor_msgs::msg::LaserScan scan_info_();
+  long int avoid_count_;
+  long int rand_count_;
+  LaserSensor laser_sensor_info_;
+
+  // Neighbor Attributes
+  std::vector<uint> nbh_ids_;               // vector to store the neighbor robot ids
+  std::vector<double> last_communication_;  // store robot's time of encounter with others (sec)
+  const double comm_delay_ = 10;
 
   // Publishers
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_cmd_vel_;
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_map_;
 
   // Subscriptions
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr sub_laser_scan_;
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_abs_pose_;
+  std::array<rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr, 3> sub_map_;
+
+  // Services
+  rclcpp::Client<levy_walk_msgs::srv::Neighbors>::SharedPtr srv_nbh_client_;
 
   // Timer
   rclcpp::TimerBase::SharedPtr timer_;
 
 public:
   // Constructor
-  Robot(uint robot_id_, std::string robot_name_, uint no_of_robots=1,
+  Robot(uint robot_id_, std::string robot_name_, std::string srv_name_nbh,
+        uint no_of_robots=1,
         double radial_noise=0.01,
         double sensing_radius_=0.40);
 
-  // Robot Attribute Encapsulation
+  // Robot Attribute Methods
   void set_turn_speed(double new_speed);
+  void update_neighbors();
 
   // Robot Operations
   void move();
@@ -77,6 +117,14 @@ public:
 
   // Grid map
   mapmerge::grid_map g_map;
+
+  // Node Attribues
+  bool verbose = false;
+
+  // Timer Attributes
+  double static_time;
+  long unsigned int pre_time;
+  int count;
 
 };
 
--- src/robot.cpp
+++ src/robot.cpp
@@ -1,10 +1,14 @@
 #include "maps_hough/robot.hpp"
 
 
+using namespace std::chrono_literals;
+
+
 Robot::Robot(uint robot_id_,
              std::string robot_name_,
+             std::string srv_name_nbh,
              uint no_of_robots,
-             double radial_noise,
+             double radial_noise_,
              double sensing_radius_):
     Node("robot_node"),
     id_{robot_id_},
@@ -13,21 +17,20 @@ Robot::Robot(uint robot_id_,
     y{0.0},
     w{0.0},
     sensing_radius_{sensing_radius_},
-    last_communication(no_of_robots),
-    avoid_count{0},
-    rand_count{0}
+    last_communication_(no_of_robots),
+    avoid_count_{0},
+    rand_count_{0}
 
 /**
  * Constructor for the robot class
  * @param robot_id_ : the id number of the robot
  * @param robot_name_ : the common name for the robot
- * @param planner_ : the planner object pointer
- * @param radial_noise : the variance of the radial noise of the range sensor
+ * @param radial_noise_ : the variance of the radial noise of the range sensor
  * @param sensing_radius_ : the sensing radius of the robot
  */
 {
   std:: string robot_name_aug = robot_name_ + std::to_string(robot_id_);
-  laser_sensor.range_noise_const = radial_noise;
+  laser_sensor_info_.range_noise_const = radial_noise_;
 
   // Setting up the subscriber to laser ranger sensors
   sub_laser_scan_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
@@ -42,17 +45,17 @@ Robot::Robot(uint robot_id_,
   {
     return [this, i](sensor_msgs::msg::Image::SharedPtr msg) -> void
     {
-      RCLCPP_INFO(this->get_logger(), "TEST UMC%d Size of nbh_ids: %d", i, nbh_ids.size());
-      if (!nbh_ids.empty() && std::find(nbh_ids.begin(), nbh_ids.end(), i) != nbh_ids.end())
+      RCLCPP_INFO(this->get_logger(), "TEST UMC%d Size of nbh_ids: %d", i, nbh_ids_.size());
+      if (!nbh_ids_.empty() && std::find(nbh_ids_.begin(), nbh_ids_.end(), i) != nbh_ids_.end())
       {
         RCLCPP_INFO(get_logger(), "Updating Map");
         
         // check if ample time has past since the map merger
-        if (last_communication[i] + comm_delay < this->now().seconds())
+        if (last_communication_[i] + comm_delay_ < this->now().seconds())
         {
           RCLCPP_INFO(get_logger(), "Merging the maps of robots %d and %d", id_, i);
           merger(occ_grid_map->og_, cv_bridge::toCvShare(msg, "mono8")->image);
-          last_communication[i] = this->now().seconds();
+          last_communication_[i] = this->now().seconds();
         }
       }
     };
@@ -73,7 +76,7 @@ Robot::Robot(uint robot_id_,
   pub_cmd_vel_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
 
   // Setting up the client for neighbor service
-  get_nbh_client_ = this->create_client<levy_walk_msgs::srv::Neighbors>(nbh_service_name);
+  srv_nbh_client_ = this->create_client<levy_walk_msgs::srv::Neighbors>(srv_name_nbh);
 
   // Set up timer
   timer_ = this->create_wall_timer(500ms, std::bind(&Robot::timer_callback, this));
@@ -93,34 +96,34 @@ void Robot::laser_scan_callback(sensor_msgs::msg::LaserScan::SharedPtr msg)
  * Turtlebot3 Burger.
  */
 {
-  if (laser_sensor.ranges.size() == 0)  // initialize the object
+  if (laser_sensor_info_.ranges.size() == 0)  // initialize the object
   {
-    laser_sensor.angle.max = msg->angle_max;
-    laser_sensor.angle.min = msg->angle_min;
-    laser_sensor.range.max = 0.4;  // msg->range_max;
-    laser_sensor.range.min = msg->range_min;
+    laser_sensor_info_.angle.max = msg->angle_max;
+    laser_sensor_info_.angle.min = msg->angle_min;
+    laser_sensor_info_.range.max = 0.4;  // msg->range_max;
+    laser_sensor_info_.range.min = msg->range_min;
     double temp_range = 0.0;
     
     for (double b = 100 * msg->angle_increment; b >= msg->angle_min; b -= msg->angle_increment)
     {
-      laser_sensor.bearings.emplace_back(b);
+      laser_sensor_info_.bearings.emplace_back(b);
     }
     
     for (double b = msg->angle_max; b >= 260 * msg->angle_increment; b -= msg->angle_increment)
     {
-      laser_sensor.bearings.emplace_back(b);
+      laser_sensor_info_.bearings.emplace_back(b);
     }
 
     int no_of_beams = ((msg->angle_max-msg->angle_min)/msg->angle_increment+1) / 2;
     
     for (auto i=90; i>=0; i--)
     {
-      laser_sensor.ranges.emplace_back(static_cast<double>(msg->ranges[i]));
+      laser_sensor_info_.ranges.emplace_back(static_cast<double>(msg->ranges[i]));
     }
 
     for (auto i=359; i>=270; i--)
     {
-      laser_sensor.ranges.emplace_back(static_cast<double>(msg->ranges[i]));
+      laser_sensor_info_.ranges.emplace_back(static_cast<double>(msg->ranges[i]));
     }
 
   }
@@ -132,7 +135,7 @@ void Robot::laser_scan_callback(sensor_msgs::msg::LaserScan::SharedPtr msg)
       // ROS_INFO("laser sensor %d: %.3f", i, msg->ranges[i]);
       if (msg->ranges[j] > 0.4)
       {
-        laser_sensor.ranges[i] = 0.4;
+        laser_sensor_info_.ranges[i] = 0.4;
       // This `else if` statement is a way to filter 0 readings by the lidar
       }
       else if (msg->ranges[j] <= 0.02)
@@ -142,18 +145,18 @@ void Robot::laser_scan_callback(sensor_msgs::msg::LaserScan::SharedPtr msg)
         {
           if (msg->ranges[j+k] > 0.02)
           {
-            laser_sensor.ranges[i] = (msg->ranges[j+k] > 0.4) ? 0.4 : msg->ranges[j+k];
+            laser_sensor_info_.ranges[i] = (msg->ranges[j+k] > 0.4) ? 0.4 : msg->ranges[j+k];
             break;
           }
           else if (k == 3)
           {
-            laser_sensor.ranges[i] = 0.36;
+            laser_sensor_info_.ranges[i] = 0.36;
           }
         }
       }
       else
       {
-        laser_sensor.ranges[i] = msg->ranges[j];
+        laser_sensor_info_.ranges[i] = msg->ranges[j];
       }
       j--;
     }
@@ -164,7 +167,7 @@ void Robot::laser_scan_callback(sensor_msgs::msg::LaserScan::SharedPtr msg)
       // ROS_INFO("laser sensor %d: %.3f", i, msg->ranges[i]);
       if (msg->ranges[j] > 0.4)
       {
-        laser_sensor.ranges[i] = 0.4;
+        laser_sensor_info_.ranges[i] = 0.4;
       // This `else if` statement is a way to filter 0 readings by the lidar
       }
       else if (msg->ranges[j] <= 0.02)
@@ -174,18 +177,18 @@ void Robot::laser_scan_callback(sensor_msgs::msg::LaserScan::SharedPtr msg)
         {
           if (msg->ranges[j-k] > 0.02)
           {
-            laser_sensor.ranges[i] = (msg->ranges[j-k] > 0.4) ? 0.4 : msg->ranges[j-k];
+            laser_sensor_info_.ranges[i] = (msg->ranges[j-k] > 0.4) ? 0.4 : msg->ranges[j-k];
             break;
           }
           else if (k == 3)
           {
-            laser_sensor.ranges[i] = 0.36;
+            laser_sensor_info_.ranges[i] = 0.36;
           }
         }
       }
       else
       {
-        laser_sensor.ranges[i] = msg->ranges[j];
+        laser_sensor_info_.ranges[i] = msg->ranges[j];
       }
       j--;
     }
@@ -221,116 +224,86 @@ void Robot::move()
  * the method to move the robot
  */
 {
-  // The function moves the robot according to the planner object
-
-  // Tolerance to check the various angle and time conditions
-  const double rad_tol = 2 * M_PI / 180;
-  const double time_tol = 0.1;
-  static NS_my_planner::MOTION_MODES currentMode = NS_my_planner::MOTION_MODES::START;
-  
-  // variables for obstacle avoidance
-  static const double stopDist = 0.20;  // stopping distance of the robot (original 0.3)
-  static const int avoidDuration = 50;  // duration to perform obstacle avoidance (original 10)
-  static const double avoidSpeed = 0.01;  // original 0.05
-  static const double avoidTurn = 0.175;  // original 0.5
-  static const double minFrontDistance = 0.35;  // original 1.0
-  bool obstruction = false;  // flag for detecting obstruction
-  bool stop = false;  // flag to stop the robot
+  // Variables for obstacle avoidance
+  static const double stop_dist = 0.20;           // stopping distance of the robot (original 0.3)
+  static const int avoid_duration = 50;           // duration to perform obstacle avoidance (original 10)
+  static const double avoid_speed = 0.01;         // original 0.05
+  static const double avoid_turn = 0.175;         // original 0.5
+  static const double min_front_distance = 0.35;  // original 1.0
+  bool obstruction = false;                       // flag for detecting obstruction
+  bool stop = false;                              // flag to stop the robot
 
   // Get the laser data
-  auto& laser_scan = laser_sensor.ranges;
-  auto sample_count = laser_scan.size();
+  auto& laser_scan = laser_sensor_info_.ranges;
+  auto sample_count = laser_sensor_info_.ranges.size();
+
   if (verbose)
     RCLCPP_INFO(get_logger(), "Sample Count Laser: %lu", sample_count);
 
 
   // find the closest distance to the left and right and also check if
   // there's anything in front
-  double minleft = 100.0;  // 1e6
-  double minright = 100.0;  // 1e6
+  double min_left = 100.0;  // 1e6
+  double min_right = 100.0;  // 1e6
   double weight = 0.5;
 
   for (uint32_t i = 0; i < sample_count; i++)
   {
-  // if (verbose && 0)
-  //   printf("%.3f ", laser_scan[i]);
-
-    if ((i > (sample_count / 3)) && (i < (sample_count - (sample_count / 3))) && laser_scan[i] < minFrontDistance)
-    {
-      // if (verbose)
-      //   ROS_INFO("Obstruction!");
-      //   puts("  obstruction!");
+    if ((i > (sample_count / 3)) && (i < (sample_count - (sample_count / 3))) && laser_scan[i] < min_front_distance)
       obstruction = true;
-    }
-    // ROS_INFO("%.3f", laser_scan[i]);
-    // if (laser_scan[i] < minFrontDistance) {
-      // if (verbose)
-      //  ROS_INFO("Obstruction!");
-      // obstruction = true;
-      // stop = true;
-    // }
 
-    if (laser_scan[i] < stopDist)
-    {
-      // if (verbose)
-      //   ROS_INFO("Stopping!");
-      //   puts("  stopping!");
+    if (laser_scan[i] < stop_dist)
       stop = true;
-    }
 
     if (i < sample_count / 2)
-    {
-      minleft = std::min(minleft, laser_scan[i]);
-    }
+      min_left = std::min(min_left, laser_scan[i]);
     else
-    {
-      minright = std::min(minright, laser_scan[i]);
-    }
+      min_right = std::min(min_right, laser_scan[i]);
   }
 
   if (verbose)
   {
-    RCLCPP_INFO(get_logger(), "min left: %.3f", minleft);
-    RCLCPP_INFO(get_logger(), "min right: %.3f", minright);
+    RCLCPP_INFO(get_logger(), "min left: %.3f", min_left);
+    RCLCPP_INFO(get_logger(), "min right: %.3f", min_right);
   }
 
-  if (obstruction || stop || (avoid_count > 0))
+  if (obstruction || stop || (avoid_count_ > 0))
   {
     if (verbose)
-      RCLCPP_INFO(get_logger(), "Avoid: %ld", avoid_count);
+      RCLCPP_INFO(get_logger(), "Avoid: %ld", avoid_count_);
 
     // setting the robot to stop
-    this->cmd_vel_msg.linear.x = stop ? 0.0 : avoidSpeed;
+    this->cmd_vel_msg.linear.x = stop ? 0.0 : avoid_speed;
     pub_cmd_vel_->publish(this->cmd_vel_msg);
 
     // once we start avoiding, select a turn direction and stick
     // with it for a few iterations
-    if (avoid_count < 1)
+    if (avoid_count_ < 1)
     {
       if (verbose)
         RCLCPP_INFO(get_logger(), "Avoid START");
       
-      avoid_count = random() % (avoidDuration - 10);
+      avoid_count_ = random() % (avoid_duration - 10);
 
       // right -; left +;
-      if (minleft < minright)
+      if (min_left < min_right)
       {
-        set_turn_speed(-avoidTurn);
+        set_turn_speed(-avoid_turn);
         if (verbose)
-          RCLCPP_INFO(get_logger(), "Turning right: %.2f", -avoidTurn);
+          RCLCPP_INFO(get_logger(), "Turning right: %.2f", -avoid_turn);
       }
       else
       {
-        set_turn_speed(+avoidTurn);
+        set_turn_speed(+avoid_turn);
         if (verbose)
-          RCLCPP_INFO(get_logger(), "Turning left: %.2f", +avoidTurn);
+          RCLCPP_INFO(get_logger(), "Turning left: %.2f", +avoid_turn);
       }
-      publish();
+      pub_cmd_vel_->publish(this->cmd_vel_msg);
     }
 
-    minleft = 100.0;
-    minright = 100.0;
-    avoid_count--;
+    min_left = 100.0;
+    min_right = 100.0;
+    avoid_count_--;
 
   }
   else
@@ -351,6 +324,36 @@ void Robot::set_turn_speed(double new_speed)
  */
 {
   this->cmd_vel_msg.angular.z = new_speed;
+}
+
+
+void Robot::update_neighbors()
+/**
+ * the method to update the neighbours of a robot using a ros2 service
+ */
+{
+  while (!srv_nbh_client_->wait_for_service(1s)) {
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
+      return;
+    }
+    RCLCPP_INFO(this->get_logger(), "service not available, waiting again...");
+  }
+  
+  auto request = std::make_shared<levy_walk_msgs::srv::Neighbors::Request>();
+  request->robot_id = id_;
+  request->radius = sensing_radius_;
+
+  using ServiceResponseFuture = rclcpp::Client<levy_walk_msgs::srv::Neighbors>::SharedFuture;
+  
+  auto response_received_callback = [this](ServiceResponseFuture future) {
+    auto result = future.get();
+    RCLCPP_INFO(this->get_logger(), "Result recieved from server!");
+    nbh_ids_.assign(result->neighbors.begin(), result->neighbors.end());
+  };
+  
+
+  auto future_result = srv_nbh_client_->async_send_request(request, response_received_callback);
 }
 
 
