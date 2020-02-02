60a9fc293edd976199523d1aa5a03ca6f8aedd56
zmk5 <zkakish@gmail.com>
2020-02-01 22:53:26 -0700

    removed ragesh occupancy grid code

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 
+#include "maps_hough/robot.hpp"
 #include "maps_hough/command_line_parser.hpp"
 
 
@@ -36,55 +37,25 @@ int main(int argc, char** argv)
   // The parameters for map object
   const double min_x = -1.38;        // in meters (original -8) // -1.3  // -1.5
   const double min_y = -1.07;        // in meters (original -8) // -0.8  // -1.5
-  const double cell_size_x = 0.02;  // in meters (original 0.02)
-  const double cell_size_y = 0.02;  // in meters (original 0.02)
-  const int n_cell_x = 138;         // no of cells along x (original 800) // 130  // 150
-  const int n_cell_y = 107;         // no of cells along y (original 800)  // 80  // 150
-
-  // Create a map object
-  map::occupancyGrid2D<double, int> occ_grid{
-    min_x, min_y, cell_size_x, cell_size_y, n_cell_x, n_cell_y
-  };
+  const double cell_size_x = 0.02;   // in meters (original 0.02)
+  const double cell_size_y = 0.02;   // in meters (original 0.02)
+  const int n_cell_x = 138;          // no of cells along x (original 800) // 130  // 150
+  const int n_cell_y = 107;          // no of cells along y (original 800)  // 80  // 150
 
   // Robot speed parameters
   static const double cruisesSpeed = 0.05;  // 0.05 original 0.4
   static const double turnSpeed = 0.1;      // 0.1 original 0.2
 
-  // Velocity object
-  Velocity velocity;
-  velocity.linear.x = cruisesSpeed;
-  velocity.linear.y = 0;
-  velocity.linear.z = 0;
-  velocity.angular.x = 0;
-  velocity.angular.y = 0;
-  velocity.angular.z = turnSpeed;
-
-  Pose startP{
-    0.0, 0.0, 0.0
-  };
-
   // Radial range sensor noise
   double radial_noise = 0.01;
 
-  // Forward sensor model parameter
-  NS_my_planner::F_S_M_parameters fsm;
-  fsm.sigma = radial_noise;
-
-  // Create a planner object
-  NS_my_planner::base_planner planner{
-    50, 0, startP, velocity
-  };
-  NS_my_planner::MI_levyWalk_planner MI_planner{
-    0, startP, velocity, fsm, NS_my_planner::KLDMI, 5
-  };
-
   // Create a robot object
   auto robot = std::make_shared<Robot>(
-    static_cast<uint>(std::stoi(robot_number)), std::string{"turtlebot_0"}, &planner,
-    static_cast<uint>(std::stoi(no_of_robots)), radial_noise, in_test_mode);
+    static_cast<uint>(std::stoi(robot_number)), std::string{"turtlebot_0"}, "/robot_neighbors",
+    static_cast<uint>(std::stoi(no_of_robots)), radial_noise);
 
-  // Assigning the map object to the robot
-  robot->occ_grid_map = &occ_grid;
+  // Resize gridmap
+  robot->g_map.resize_map(n_cell_y, n_cell_x);
 
   rclcpp::spin(robot);
   rclcpp::shutdown();
